6bad52789475a94b39d35f0eb5c80387a6e832fd
Half-Shot <will@half-shot.uk>
2020-01-17 10:42:41 +0000

    Add settings tab
    Signed-off-by: Half-Shot <will@half-shot.uk>

--- src/components/SettingsModal.vue
+++ src/components/SettingsModal.vue
@@ -1,16 +1,65 @@
 <template>
-    <Modal title="Settings" :closeFn="closeFn">
-        <p slot="body"> Some data </p>
+    <Modal :title="`Settings - ${tabName}`" :closeFn="closeFn">
+        <div class="settings-container" slot="body">
+            <div class="nav">
+                <ul> <!-- Options -->
+                    <li :class="`button ${activeClass(0)}`">
+                        <a href="#">Keymappings</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="options">
+                <KeymappingTab v-if="tabIndex === 0">
+                </KeymappingTab>
+            </div>
+        </div>
     </Modal>
 </template>
+
+<style lang="scss" scoped>
+.settings-container {
+    column-count: 2;
+    columns: 10vw auto;
+    column-gap: 20px;
+}
+
+.nav {
+    border-right: 1px solid gray;
+    height: 100%;
+
+    ul {
+        list-style: none;
+    }
+
+    .button.active {
+        background: lightgreen;
+        border-radius: 5px;
+        padding: 5px;
+        margin-right: 5px;
+    }
+}
+</style>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import Modal from "./Modal.vue";
+import KeymappingTab from "./SettingsModalTabs/KeymappingTab.vue";
 
-@Component
+@Component({
+    components: {
+        KeymappingTab,
+    }
+})
 export default class SettingsModal extends Modal {
+    private tabIndex = 0;
 
+    private activeClass(tab: number) {
+        return this.tabIndex === tab ? `active` : "";
+    }
+
+    private gotoTab(tabIndex: number) {
+        this.tabIndex = tabIndex;
+    }
 }
 
 Vue.component("SettingsModal", SettingsModal);
---
+++ src/components/SettingsModalTabs/KeymappingTab.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Keymappings
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class KeymappingsTab extends Vue {
+    private static readonly tabName = "Keymappings";
+}
+</script>
