be6aabb72f6ae3200de06c4ef5d65455b8e9956f
Half-Shot <will@half-shot.uk>
2020-01-27 16:44:29 +0000

    Allow users to logout if sync is stuck

--- src/components/Sync.vue
+++ src/components/Sync.vue
@@ -1,5 +1,8 @@
 <template>
-    <p> Syncing as <strong>{{ name }}</strong></p>
+    <main>
+        <p> Syncing as <strong>{{ name }}</strong></p>
+        <p v-if="slow">Taking too long? <router-link :to="`/logout`">Logout</router-link></p>
+    </main>
 </template>
 
 <style lang="scss" scoped>
@@ -7,17 +10,36 @@ p {
     text-align: center;
 }
 </style>
-<script>
-export default {
-    name: "sync",
-    components: { },
-    computed: {
-        name: function() {
-            if (this.$root.$data.sharedState.isGuest) {
-                return `a Guest`;
-            }
-            return this.$root.$data.sharedState.displayName || this.$root.$data.sharedState.userId;
-        },
-    },
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "app",
+})
+export default class Sync extends Vue {
+    private slow = false;
+
+    get name() {
+        if (this.$root.$data.sharedState.isGuest) {
+            return `a Guest`;
+        }
+        return this.$root.$data.sharedState.displayName || this.$root.$data.sharedState.userId;
+    }
+
+    private beforeMount() {
+        this.slow = false;
+        if (this.$root.$data.sharedState.isGuest) {
+            return; // Do not show logout for guests.
+        }
+        setInterval(() => {
+            this.slow = true;
+        }, 5000);
+    }
+
+    private beforeDestroy() {
+        if (this._timer) {
+            clearInterval(this._timer);
+        }
+    }
 };
 </script>
